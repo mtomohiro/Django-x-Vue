4d6a591873a0aad1d93bce8e14b9260fb3c7af83
minami tomohiro <mtomohiro.archives@gmail.com>
2020-06-28 00:17:38 +0900

    いろいろ検証中

---
+++ QubeWeb/dq/static/dq/component/headerColumn.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <span>{{ message }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'message'
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
